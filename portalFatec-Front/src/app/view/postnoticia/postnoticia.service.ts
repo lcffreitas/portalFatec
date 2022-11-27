050c8ea82f27c28cbe30bd65d6c15dfc6c2a5f78
lcffreitas <lcffreitas1@gmail.com>
2022-11-27 15:55:28 -0300

    ultima versão portal de notícias

--- portalFatec-Front/src/app/view/postnoticia/postnoticia.service.ts
+++ portalFatec-Front/src/app/view/postnoticia/postnoticia.service.ts
@@ -11,26 +11,7 @@ export class PostnoticiaService {
   constructor(private http:HttpClient) { }
 
   getNoticias(){
-    //return this.getNoticiasMock();
     return this.http.get<Noticia[]>(this.API);
   }
-
-  getNoticiasMock() {
-    let noticias:Noticia[] = [];
-    let n1:Noticia = new Noticia();
-    n1.id='1';
-    n1.descricao ='desc1';
-    n1.titulo = 'n1';
-
-    let n2:Noticia = new Noticia();
-    n2.id='2';
-    n2.descricao ='desc2';
-    n2.titulo = 'n2';
-
-    noticias.push(n1,n2);
-
-    return noticias;
-  }
-
 
 }
